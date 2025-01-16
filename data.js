bc8c683c6aab090a74d745f743bb98c22e44efcc
pqrad1se <mrazor110@mail.ru>
2025-01-16 23:02:29 +0300

    tc_6_2

---
+++ data.js
@@ -0,0 +1,33 @@
+var data = [{
+    title: 'Юпитер',
+    nick: 'jupiter',
+    avatar: '/images/jupiter.jpg',
+    desc: 'Пятая планета от Солнца и самая большая в Солнечной системе.'
+},
+{
+    title: 'Марс',
+    nick: 'mars',
+    avatar: '/images/mars.jpg',
+    desc: 'Четвёртая планета от Солнца, известна как Красная планета из-за своего цвета.'
+},
+{
+    title: 'Меркурий',
+    nick: 'mercury',
+    avatar: '/images/mercury.jpg',
+    desc: 'Ближайшая планета к Солнцу и самая маленькая в Солнечной системе.'
+},
+{
+    title: 'Нептун',
+    nick: 'neptune',
+    avatar: '/images/neptune.jpg',
+    desc: 'Восьмая и самая дальняя от Солнца планета, известная своими мощными ветрами.'
+},
+{
+    title: 'Уран',
+    nick: 'uranus',
+    avatar: '/images/uranus.jpg',
+    desc: 'Седьмая планета от Солнца, вращается с наклоном на бок.'
+}
+
+];
+module.exports.data = data;
