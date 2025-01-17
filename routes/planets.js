0fa8ba0e4a10e32fa390562a121f435f09aa8de2
pqrad1se <mrazor110@mail.ru>
2025-01-18 02:24:30 +0300

    tc_8_3

--- routes/planets.js
+++ routes/planets.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Planet = require('../models/planet').Planet;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,8 +8,16 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница планет */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+router.get("/:nick", async function(req, res, next) {
+    var planets = await Planet.find({nick: req.params.nick});
+    console.log(planets)
+    if(!planets.length) return next(new Error("Нет такой планеты в Солнечной системе"))
+    var planet = planets[0];
+    res.render('planets', {
+    title: planet.title,
+    picture: planet.avatar,
+    desc: planet.desc
+    })
     });
 
 module.exports = router;
