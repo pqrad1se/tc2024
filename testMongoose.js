19c7c785bbc2d4c7c369c757e7a55746103796e0
pqrad1se <mrazor110@mail.ru>
2025-01-18 00:09:34 +0300

    tc_7_3

--- testMongoose.js
+++ testMongoose.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
-var schema = mongoose.Schema({ name: String })
+var Planet = require('./models/planet.js').Planet
 
-schema.methods.showPlanet = function() {
-    console.log("Планета: " + this.name );
-  };
+var planet = new Planet ({
+    title: "Юпитер",
+    nick: "jupiter",
 
-const Planet = mongoose.model('Planet', schema);
-const system = new Planet({ name: 'Юпитер' });
-system.save().then(() => system.showPlanet());
+})
+planet.save (); 
+
 
