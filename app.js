e8d347b1758299cbaef33ed6a16740722c20ebad
pqrad1se <mrazor110@mail.ru>
2025-01-18 22:57:08 +0300

    tc_10_5

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use(session({
     next()
     })
     app.use(require("./middlewares/createMenu.js"))
+    app.use(require("./middlewares/createUser.js"))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
