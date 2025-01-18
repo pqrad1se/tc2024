5766169f17b015a6da63c5ffa5e94e94bf054fc1
pqrad1se <mrazor110@mail.ru>
2025-01-18 18:38:42 +0300

    tc_8_4

--- routes/index.js
+++ routes/index.js
@@ -6,52 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* Страница Юпитера */
-router.get('/jupiter', function(req, res, next) {
-  res.render('planets', {
-    title: "Юпитер",
-    picture: "images/jupiter.jpg",
-    desc: "Пятая планета от Солнца и самая большая в Солнечной системе."
-    });
-  });
 
-
-/* Страница Марса */
-router.get('/mars', function(req, res, next) {
-  res.render('planets', {
-      title: "Марс",
-      picture: "images/mars.jpg",
-      desc: "Четвёртая планета от Солнца, известна как 'Красная планета' из-за своего цвета."
-    });
-  });
-
-
-  /* Страница Меркурия */
-router.get('/mercury', function(req, res, next) {
-  res.render('planets', {
-      title: "Меркурий",
-      picture: "images/mercury.jpg",
-      desc: "Ближайшая планета к Солнцу и самая маленькая в Солнечной системе."
-  });
-});
-
-
-  /* Страница Нептуна */
-router.get('/neptune', function(req, res, next) {
-  res.render('planets', {
-      title: "Нептун",
-      picture: "images/neptune.jpg",
-      desc: "Восьмая и самая дальняя от Солнца планета, известная своими мощными ветрами."
-});
-});
-
-  /* Страница Урана */
-router.get('/uranus', function(req, res, next) {
-  res.render('planets', {
-    title: "Уран",
-    picture: "images/uranus.jpg",
-    desc: "Седьмая планета от Солнца, вращается с наклоном на бок."
-});
-});
 
 module.exports = router;
