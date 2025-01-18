1b49207076205fe20a4cbd65aad3bea79aa0d240
pqrad1se <mrazor110@mail.ru>
2025-01-18 20:03:59 +0300

    tc_9_3

--- app.js
+++ app.js
@@ -23,12 +23,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var MongoStore = require('connect-mongo');
 app.use(session({
   secret: "SolarSystem",
   cookie:{maxAge:60*1000},
   proxy: true,
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true, 
+  store: MongoStore.create({mongoUrl: 'mongodb://localhost/tc2024'})
   }))
 
 app.use('/', indexRouter);
